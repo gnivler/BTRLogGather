2d167e450d624c3a4c367ebc290416896f14e492
gnivler <gnivil@gmail.com>
2020-07-09 08:08:43 -0600

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BTRLogGather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gather = new System.Windows.Forms.Button();
+            this.Show = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            this.Gather.Location = new System.Drawing.Point(12, 12);
+            this.Gather.Name = "Gather";
+            this.Gather.Size = new System.Drawing.Size(169, 28);
+            this.Gather.TabIndex = 0;
+            this.Gather.Text = "Gather";
+            this.Gather.UseVisualStyleBackColor = true;
+            this.Gather.Click += new System.EventHandler(this.Gather_Click);
+            this.Show.Location = new System.Drawing.Point(191, 12);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(169, 28);
+            this.Show.TabIndex = 0;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            this.Play.Location = new System.Drawing.Point(12, 46);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(348, 28);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 87);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.Gather);
+            this.Name = "Form1";
+            this.Text = "BTRLogGather 1.0";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button Gather;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Show;
+
+        #endregion
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BTRLogGather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Gather_Click(object sender, EventArgs e)
+        {
+            var temp = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "BTRLogGather\\");
+            var timeStamp = DateTime.Now.ToLongTimeString().Replace(" ", "").Replace(":", "");
+            temp = Path.Combine(temp, timeStamp);
+            if (!Directory.Exists(temp))
+            {
+                Directory.CreateDirectory(temp);
+            }
+            var dialog = new OpenFileDialog
+            {
+                Title = "Please select your BattleTech.exe file in the game folder",
+                Filter = "BattleTech.exe|BattleTech.exe",
+            };
+            dialog.ShowDialog();
+            if (dialog.FileName.ToLower().EndsWith("battletech.exe") )
+            {
+                var root = new FileInfo(dialog.FileName).DirectoryName;
+                var modtek = Path.Combine(root, "mods/.modtek");
+                File.Copy(Path.Combine(root, "mods/output_log.txt"), Path.Combine(temp, "output_log.txt"), true);
+                File.Copy(Path.Combine(root, "mods/cleaned_output_log.txt"), Path.Combine(temp, "cleaned_output_log.txt"), true);
+                File.Copy(Path.Combine(modtek, "harmony_summary.log"), Path.Combine(temp, "harmony_summary.log"), true);
+                File.Copy(Path.Combine(modtek, "ModTek.log"), Path.Combine(temp, "ModTek.log"), true);
+            }
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
