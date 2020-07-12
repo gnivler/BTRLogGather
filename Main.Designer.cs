2b70843e3d90f3691ecfbb5ce9b702f1157c4595
gnivler <gnivil@gmail.com>
2020-07-11 19:26:10 -0600

    1.0.2

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BTRLogGather
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -29,44 +29,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Gather = new System.Windows.Forms.Button();
-            this.Show = new System.Windows.Forms.Button();
-            this.Play = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.gatherButton = new System.Windows.Forms.Button();
+            this.showOutputButton = new System.Windows.Forms.Button();
+            this.playGameButton = new System.Windows.Forms.Button();
+            this.detailedLogsCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
-            this.Gather.Location = new System.Drawing.Point(12, 12);
-            this.Gather.Name = "Gather";
-            this.Gather.Size = new System.Drawing.Size(169, 28);
-            this.Gather.TabIndex = 0;
-            this.Gather.Text = "Gather";
-            this.Gather.UseVisualStyleBackColor = true;
-            this.Gather.Click += new System.EventHandler(this.Gather_Click);
-            this.Show.Location = new System.Drawing.Point(191, 12);
-            this.Show.Name = "Show";
-            this.Show.Size = new System.Drawing.Size(169, 28);
-            this.Show.TabIndex = 0;
-            this.Show.Text = "Show";
-            this.Show.UseVisualStyleBackColor = true;
-            this.Show.Click += new System.EventHandler(this.Show_Click);
-            this.Play.Location = new System.Drawing.Point(12, 46);
-            this.Play.Name = "Play";
-            this.Play.Size = new System.Drawing.Size(348, 28);
-            this.Play.TabIndex = 1;
-            this.Play.Text = "Play";
-            this.Play.UseVisualStyleBackColor = true;
+            // 
+            // gatherButton
+            // 
+            this.gatherButton.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.gatherButton.Location = new System.Drawing.Point(12, 12);
+            this.gatherButton.Name = "gatherButton";
+            this.gatherButton.Size = new System.Drawing.Size(131, 28);
+            this.gatherButton.TabIndex = 0;
+            this.gatherButton.Text = "Gather Logs";
+            this.gatherButton.UseVisualStyleBackColor = true;
+            this.gatherButton.Click += new System.EventHandler(this.Gather_Click);
+            // 
+            // showOutputButton
+            // 
+            this.showOutputButton.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.showOutputButton.Location = new System.Drawing.Point(282, 12);
+            this.showOutputButton.Name = "showOutputButton";
+            this.showOutputButton.Size = new System.Drawing.Size(78, 28);
+            this.showOutputButton.TabIndex = 2;
+            this.showOutputButton.Text = "Show Logs";
+            this.showOutputButton.UseVisualStyleBackColor = true;
+            this.showOutputButton.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // playGameButton
+            // 
+            this.playGameButton.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.playGameButton.Location = new System.Drawing.Point(12, 46);
+            this.playGameButton.Name = "playGameButton";
+            this.playGameButton.Size = new System.Drawing.Size(348, 28);
+            this.playGameButton.TabIndex = 3;
+            this.playGameButton.Text = "Play BattleTech";
+            this.playGameButton.UseVisualStyleBackColor = true;
+            this.playGameButton.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // detailedLogsCheckBox
+            // 
+            this.detailedLogsCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.detailedLogsCheckBox.Checked = true;
+            this.detailedLogsCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.detailedLogsCheckBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.detailedLogsCheckBox.Location = new System.Drawing.Point(149, 16);
+            this.detailedLogsCheckBox.Name = "detailedLogsCheckBox";
+            this.detailedLogsCheckBox.Size = new System.Drawing.Size(118, 22);
+            this.detailedLogsCheckBox.TabIndex = 4;
+            this.detailedLogsCheckBox.Text = "Detailed logs";
+            this.detailedLogsCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 87);
-            this.Controls.Add(this.Play);
-            this.Controls.Add(this.Show);
-            this.Controls.Add(this.Gather);
-            this.Name = "Form1";
-            this.Text = "BTRLogGather 1.0";
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(781, 383);
+            this.Controls.Add(this.detailedLogsCheckBox);
+            this.Controls.Add(this.playGameButton);
+            this.Controls.Add(this.showOutputButton);
+            this.Controls.Add(this.gatherButton);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(388, 126);
+            this.Name = "Main";
+            this.Text = "BTRLogGather 1.0.2";
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Button Gather;
-        private System.Windows.Forms.Button Play;
-        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.CheckBox detailedLogsCheckBox;
+        private System.Windows.Forms.Button gatherButton;
+        private System.Windows.Forms.Button playGameButton;
+        private System.Windows.Forms.Button showOutputButton;
 
         #endregion
     }
--- Main.cs
+++ Main.cs
@@ -1,45 +1,155 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BTRLogGather
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        private static string battleTechExe = File.Exists(Environment.CurrentDirectory + "\\battletech.exe")
+            ? new FileInfo(Environment.CurrentDirectory + "\\battletech.exe").FullName
+            : default;
+
+        private static readonly string MainDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "BTRLogGather\\");
+        private static string guidDir = Guid.NewGuid().ToString();
+
+        private static readonly string[] OmitStrings =
+        {
+            "readme",
+            "license",
+            "licence",
+            "how to",
+        };
+
+        public Main()
         {
             InitializeComponent();
+            if (string.IsNullOrEmpty(battleTechExe))
+            {
+                var parentDir = new DirectoryInfo(Environment.CurrentDirectory).Parent?.FullName;
+                battleTechExe = File.Exists($"{parentDir}\\battletech.exe")
+                    ? new FileInfo($"{parentDir}\\battletech.exe").FullName
+                    : default;
+            }
         }
 
         private void Gather_Click(object sender, EventArgs e)
         {
-            var temp = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "BTRLogGather\\");
-            var timeStamp = DateTime.Now.ToLongTimeString().Replace(" ", "").Replace(":", "");
-            temp = Path.Combine(temp, timeStamp);
-            if (!Directory.Exists(temp))
+            CreateMainDir();
+            GetBattleTechExe();
+            if (battleTechExe.EndsWith("battletech.exe"))
             {
-                Directory.CreateDirectory(temp);
+                var path = Path.Combine(MainDir, guidDir);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                var root = new FileInfo(battleTechExe).DirectoryName;
+                var modtek = Path.Combine(root, "mods/.modtek");
+                var appDataOutputLog = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}Low\\Harebrained Schemes\\BATTLETECH\\output_log.txt";
+                if (detailedLogsCheckBox.Checked)
+                {
+                    var logs = Directory.EnumerateFiles(Path.Combine(root, "mods"), "*.txt", SearchOption.AllDirectories);
+                    logs = logs.Concat(Directory.EnumerateFiles(Path.Combine(root, "mods"), "*.log", SearchOption.AllDirectories));
+                    foreach (var file in logs)
+                    {
+                        var lower = file.ToLower();
+                        if (!OmitStrings.Any(x => lower.Contains(x)))
+                        {
+                            var subDir = Regex.Match(lower, @"\\(\w*)\\\w*\.(txt|log)").Groups[1].ToString();
+                            subDir = subDir == "mods" ? "" : subDir;
+                            var targetFile = $"{MainDir}\\{guidDir}\\{subDir}\\{lower.Split('\\').Last()}";
+                            if (!Directory.Exists(targetFile))
+                            {
+                                Directory.CreateDirectory($"{MainDir}\\{guidDir}\\{subDir}");
+                            }
+
+                            File.Copy(lower, targetFile);
+                        }
+                    }
+                }
+                else
+                {
+                    File.Copy(Path.Combine(root, "mods/output_log.txt"), Path.Combine(path, "output_log.txt"), true);
+                    File.Copy(Path.Combine(root, "mods/cleaned_output_log.txt"), Path.Combine(path, "cleaned_output_log.txt"), true);
+                    File.Copy(Path.Combine(modtek, "harmony_summary.log"), Path.Combine(path, "harmony_summary.log"), true);
+                    File.Copy(Path.Combine(modtek, "ModTek.log"), Path.Combine(path, "ModTek.log"), true);
+                }
+
+                var tmp = Path.Combine(path, "HBS");
+                if (!Directory.Exists(tmp))
+                {
+                    Directory.CreateDirectory(tmp);
+                }
+
+                File.Copy(appDataOutputLog, Path.Combine(path, "HBS\\output_log.txt"), true);
+                PackageZip(path, guidDir);
+                Process.Start(MainDir);
             }
+        }
+
+        private static void PackageZip(string path, string guidDir)
+        {
+            ZipFile.CreateFromDirectory(path, $"{MainDir}\\{DateTime.Now.ToString("yyyy-M-d HHmmssss")}.zip");
+            Directory.Delete(Path.Combine(MainDir, guidDir), true);
+        }
+
+        private static void CreateMainDir()
+        {
+            if (!Directory.Exists(MainDir))
+            {
+                Directory.CreateDirectory(MainDir);
+            }
+        }
+
+        private static void GetBattleTechExe()
+        {
             var dialog = new OpenFileDialog
             {
-                Title = "Please select your BattleTech.exe file in the game folder",
-                Filter = "BattleTech.exe|BattleTech.exe",
+                Title = @"Please select your BattleTech.exe file in the game folder",
+                Filter = @"BattleTech.exe|BattleTech.exe",
             };
-            dialog.ShowDialog();
-            if (dialog.FileName.ToLower().EndsWith("battletech.exe") )
+            if (string.IsNullOrEmpty(battleTechExe) ||
+                !battleTechExe.ToLower().EndsWith("battletech.exe"))
             {
-                var root = new FileInfo(dialog.FileName).DirectoryName;
-                var modtek = Path.Combine(root, "mods/.modtek");
-                File.Copy(Path.Combine(root, "mods/output_log.txt"), Path.Combine(temp, "output_log.txt"), true);
-                File.Copy(Path.Combine(root, "mods/cleaned_output_log.txt"), Path.Combine(temp, "cleaned_output_log.txt"), true);
-                File.Copy(Path.Combine(modtek, "harmony_summary.log"), Path.Combine(temp, "harmony_summary.log"), true);
-                File.Copy(Path.Combine(modtek, "ModTek.log"), Path.Combine(temp, "ModTek.log"), true);
+                dialog.ShowDialog();
+                battleTechExe = dialog.FileName.ToLower();
             }
         }
 
         private void Show_Click(object sender, EventArgs e)
         {
-            
+            if (Directory.Exists(MainDir))
+            {
+                Process.Start(MainDir);
+            }
+            else
+            {
+                MessageBox.Show("No folder created yet", "", MessageBoxButtons.OK);
+            }
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(battleTechExe) &&
+                battleTechExe.ToLower().EndsWith("battletech.exe"))
+            {
+                Process.Start(battleTechExe);
+            }
+            else if (string.IsNullOrEmpty(battleTechExe) ||
+                     !battleTechExe.EndsWith("battletech.exe"))
+            {
+                GetBattleTechExe();
+                if (battleTechExe.EndsWith("battletech.exe"))
+                {
+                    Process.Start(battleTechExe);
+                }
+            }
         }
     }
 }
